685292efc72046896167f3a79ad3b57adb9ef156
kimssumin <ksumin0730@naver.com>
2023-07-07 13:49:12 +0900

    [#2] feat: 검색바 화면 구성

---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,10 @@
+import SearchBox from '../components/Search/SearchBox.tsx';
+
+function SearchPage() {
+  return (
+    <>
+      <SearchBox></SearchBox>
+    </>
+  );
+}
+export default SearchPage;
