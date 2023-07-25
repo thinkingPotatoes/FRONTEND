2fc12ca951a29dc0ebce20d7428f079470f059b3
이국신 <nw4611@naver.com>
2023-07-25 12:26:23 +0900

    [#8] feat: 영화 추천 영역의 더보기 컴포넌트 구현
     영화 추천의 더보기 버튼을 눌렀을 때 뜨는 창을 구현

---
+++ src/components/home/MovieRecommendDetail.tsx
@@ -0,0 +1,67 @@
+import { styled } from 'styled-components';
+import { ReactComponent as BackArrow } from '../../assets/image/icon/backArrow.svg';
+import { ReactComponent as PosterImage } from '../../assets/image/poster/poster_lg.svg';
+import Poster from './Poster.tsx';
+
+const data = [
+  { title: '아이언맨', rating: 3.5 },
+  { title: '아이언맨', rating: 3.5 },
+  { title: '아이언맨', rating: 3.5 },
+  { title: '아이언맨', rating: 3.5 },
+  { title: '아이언맨', rating: 3.5 },
+  { title: '아이언맨', rating: 3.5 },
+];
+
+function MovieRecommendDetail() {
+  return (
+    <Container>
+      <Header>
+        <BackButton>
+          <BackArrow />
+        </BackButton>
+        <Headertitle>에엥님을 위한 추천 👀</Headertitle>
+      </Header>
+      <Main>
+        {data.map((movie) => (
+          <Poster {...movie} ImgComponent={PosterImage} />
+        ))}
+      </Main>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  font-family: 'Pretendard', sans-serif;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 101;
+  background-color: var(--main-background);
+  max-width: 370px;
+  width: 100%;
+  padding: 0 10px;
+`;
+const Header = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+  color: var(--text-emphasize);
+  padding: 11.5px 0;
+`;
+
+const Headertitle = styled.div``;
+const BackButton = styled.button`
+  position: absolute;
+  left: 16px;
+`;
+
+const Main = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 15.5px;
+  margin-top: 24px;
+  flex-wrap: wrap;
+`;
+
+export default MovieRecommendDetail;
