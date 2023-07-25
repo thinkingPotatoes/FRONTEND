6376fd02c187f32a5955d5c38ba19c4ff6c3508e
callmejeje <choije401@naver.com>
2023-07-25 19:53:26 +0900

    [#14] feat: 별점 0점 뷰만 구현

---
+++ src/components/common/RatedStar.tsx
@@ -0,0 +1,15 @@
+import { ReactComponent as BlankStarIcon } from '../../assets/image/icon/blankStar.svg';
+
+function RatedStar() {
+  return (
+    <div>
+      <BlankStarIcon width={'16px'} height={'16px'} />
+      <BlankStarIcon width={'16px'} height={'16px'} />
+      <BlankStarIcon width={'16px'} height={'16px'} />
+      <BlankStarIcon width={'16px'} height={'16px'} />
+      <BlankStarIcon width={'16px'} height={'16px'} />
+    </div>
+  );
+}
+
+export default RatedStar;
