63afc8c33b4999bd839314d353ecb97d91475701
Sumin Kim <ksumin0730@hufs.ac.kr>
2023-08-06 22:42:21 +0900

    [#21] feat: 영화 하단 Nav 바 구현
    좋아요 클릭 구현

---
+++ src/components/common/LikeBtn.tsx
@@ -0,0 +1,57 @@
+import { styled } from 'styled-components';
+import { useState, useEffect } from 'react';
+import { ReactComponent as UnLikeSvg } from '../../assets/image/icon/like.svg';
+import { ReactComponent as LikeSvg } from '../../assets/image/icon/fillLike.svg';
+
+interface LikeBtnProps {
+  count: number;
+  picked: boolean;
+}
+
+/**
+ * @param {number} count : 좋아요 수
+ * @param {boolean} picked : 좋아요 했는지 여부
+ */
+
+export const LikeBtn = ({ count, picked }: LikeBtnProps) => {
+  const [liked, setLiked] = useState(false);
+  const [nowCount, setCount] = useState(0);
+
+  useEffect(() => {
+    setLiked(picked);
+  }, [picked]);
+
+  useEffect(() => {
+    setCount(count);
+  }, [count]);
+
+  const handleLikeClick = async () => {
+    liked ? setCount(nowCount - 1) : setCount(nowCount + 1);
+    setLiked(!liked);
+  };
+
+  return (
+    <>
+      <Button onClick={handleLikeClick}>
+        {liked ? <LikeSvg /> : <UnLikeSvg />}
+        {nowCount}
+      </Button>
+    </>
+  );
+};
+
+export const Button = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 3px;
+  color: var(--text-default);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 130%; /* 18.2px */
+  letter-spacing: -0.014px;
+
+  svg {
+    width: 24px;
+    height: 24px;
+  }
+`;
---
+++ src/components/reviewDetail/ReviewBottomNav.tsx
@@ -0,0 +1,54 @@
+import { styled } from 'styled-components';
+import { LikeBtn } from '../common/LikeBtn';
+import { ReactComponent as CommentSvg } from '../../assets/image/icon/comment.svg';
+
+const ReviewBottomNav = () => {
+  return (
+    <BottomNav>
+      <IconBox>
+        <LikeBtn count={7} picked={true} />
+        <CommentBox>
+          <CommentSvg />
+          <div className="commentCnt">4</div>
+        </CommentBox>
+      </IconBox>
+    </BottomNav>
+  );
+};
+
+const BottomNav = styled.div`
+  display: flex;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 88px;
+  padding: 15px 24px;
+  align-items: flex-start;
+  flex-shrink: 0;
+  border-top: 1px solid var(--dark-border-border, #3c3c47);
+  background: var(--main-background);
+`;
+
+const IconBox = styled.div`
+  display: flex;
+  alignment-items: center;
+  gap: 10px;
+`;
+
+const CommentBox = styled.div`
+  display: flex;
+  gap: 4px;
+  align-items: center;
+  color: var(--text-default);
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 130%; /* 18.2px */
+  letter-spacing: -0.014px;
+
+  svg {
+    width: 23px;
+    height: 21px;
+  }
+`;
+export default ReviewBottomNav;
