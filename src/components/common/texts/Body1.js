5f391719db000788ba5af38547507d05a7bb1e00
callmejeje <choije401@naver.com>
2023-08-16 01:35:50 +0900

    [#23] feat: 텍스트 스타일 컴포넌트 추가

---
+++ src/components/common/texts/Body1.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Body1 = styled.div`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 130%; /* 20.8px */
+  letter-spacing: -0.016px;
+`;
+
+export default Body1;
---
+++ src/components/common/texts/Body2.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Body2 = styled.div`
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 130%; /* 18.2px */
+  letter-spacing: -0.014px;
+`;
+
+export default Body2;
