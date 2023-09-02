f8899bf4fce1ac5656a12c3bd25d896c70f725b2
이국신 <nw4611@naver.com>
2023-09-02 17:51:17 +0900

    [#32] feat: 로그인 화면 구현

---
+++ src/pages/Login.tsx
@@ -0,0 +1,58 @@
+import { styled } from 'styled-components';
+import { ReactComponent as Logo } from '../assets/image/icon/logo.svg';
+import { ReactComponent as KakaoIcon } from '../assets/image/icon/KakaoTalk_logo.svg';
+import { ReactComponent as NaverIcon } from '../assets/image/icon/Naver_logo.svg';
+import { ReactComponent as GoogleIcon } from '../assets/image/icon/Google_logo.svg';
+import SocialLoginButton from '../components/login/SocialLoginButton';
+
+function Login() {
+  return (
+    <>
+      <LogoContainer>
+        <Logo width={233} height={233} />
+        <Title>FILMO</Title>
+      </LogoContainer>
+      <LoginButtonContainer>
+        <SocialLoginButton backgroundColor="#FAE100" color="#371D1E">
+          <KakaoIcon />
+          카카오톡으로 시작하기
+        </SocialLoginButton>
+        <SocialLoginButton backgroundColor="#06BE34" color="#FFFFFF">
+          <NaverIcon />
+          네이버로 시작하기
+        </SocialLoginButton>
+        <SocialLoginButton backgroundColor="#FFFFFF" color="#4E5968">
+          <GoogleIcon />
+          구글로 시작하기
+        </SocialLoginButton>
+        <SocialLoginButton backgroundColor="#1C1C25" color="#E4E4E5">
+          이메일로 시작하기
+        </SocialLoginButton>
+      </LoginButtonContainer>
+    </>
+  );
+}
+
+const LogoContainer = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+`;
+
+const LoginButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+
+const Title = styled.div`
+  font-family: 'Azonix', sans-serif;
+  color: var(--text-emphasize);
+  font-size: 24px;
+  transform: translateY(-30px);
+`;
+
+export default Login;
