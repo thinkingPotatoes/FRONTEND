9c9eb178271265f7db50a1eae88c9cfe9cfb40f1
callmejeje <choije401@naver.com>
2023-09-04 17:04:25 +0900

    [#30] feat: 리뷰 작성 페이지 라우터 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,12 @@
-import Home from '../pages/Home.tsx';
 import Blog from '../pages/Blog.tsx';
+import Home from '../pages/Home.tsx';
+import PostReview from '../pages/PostReview.tsx';
 
 export const commonRouter = [{ path: '/', element: <Home /> }];
 
 export const publicRouter = [];
 
-export const privateRouter = [{ path: '/blog', element: <Blog /> }];
+export const privateRouter = [
+  { path: '/blog', element: <Blog /> },
+  { path: '/review/:id', element: <PostReview /> },
+];
