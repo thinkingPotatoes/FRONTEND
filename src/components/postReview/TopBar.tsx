ed03a10bda204fc5d62ecc35d217e1f7bcdf43d3
callmejeje <choije401@naver.com>
2023-09-04 17:03:48 +0900

    [#30] feat: 리뷰 작성 - 상단 바 컴포넌트 구현

---
+++ src/components/postReview/TopBar.tsx
@@ -0,0 +1,39 @@
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+import { ReactComponent as BackButton } from '../../assets/icon/angle-left-btn.svg';
+import Subtitle2 from '../common/texts/Subtitle2';
+
+function TopBar({ onClick, disabled }: { onClick: () => void; disabled: boolean }) {
+  const navigate = useNavigate();
+  const handleGoBack = () => navigate(-1);
+
+  return (
+    <TopBarWrapper>
+      <BackButtonWrapper>
+        <BackButton onClick={handleGoBack} width={24} height={24} />
+      </BackButtonWrapper>
+      <PostButton disabled={disabled} onClick={onClick}>
+        <Subtitle2>등록</Subtitle2>
+      </PostButton>
+    </TopBarWrapper>
+  );
+}
+
+const TopBarWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 56px;
+  margin: 0 -20px;
+`;
+
+const BackButtonWrapper = styled.div`
+  padding: 0px 8px;
+`;
+
+const PostButton = styled.button`
+  padding: 20px 4px;
+  color: ${(props) => (props.disabled ? 'var(--dark-grey-500)' : 'var(--main)')};
+`;
+
+export default TopBar;
