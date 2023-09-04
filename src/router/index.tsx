7de6e31c15409b9ddc8ae8a30ed11e6f1cb69f63
이국신 <nw4611@naver.com>
2023-09-04 10:16:15 +0900

    [#32] feat: 라우팅 임시 적용

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,8 +1,55 @@
 import Home from '../pages/Home.tsx';
 import Blog from '../pages/Blog.tsx';
+import Login from '../pages/Login.tsx';
+import LoginEamilInputPage from '../pages/LoginEamilInputPage.tsx';
+import LoginPasswordInputPage from '../pages/LoginPasswordInputPage.tsx';
+import EmailCheck from '../pages/EmailCheck.tsx';
+import RegisterSuccess from '../pages/RegisterSuccess.tsx';
+import RegisterEamilInputPage from '../pages/RegisterEamilInputPage.tsx';
+import RegisterPasswordInputPage from '../pages/RegisterPasswordInputPage.tsx';
+import Redirect from '../pages/Redirect.tsx';
 
-export const commonRouter = [{ path: '/', element: <Home /> }];
+export const commonRouter = [
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/login/email',
+    element: <LoginEamilInputPage />,
+  },
+  {
+    path: '/login/password',
+    element: <LoginPasswordInputPage />,
+  },
 
-export const publicRouter = [];
+  {
+    path: '/register/email',
+    element: <RegisterEamilInputPage />,
+  },
+
+  {
+    path: '/register/password',
+    element: <RegisterPasswordInputPage />,
+  },
+  {
+    path: '/register/check',
+    element: <EmailCheck />,
+  },
+  {
+    path: '/register/success',
+    element: <Redirect />,
+  },
+  {
+    path: '/register/complete',
+    element: <RegisterSuccess />,
+  },
+  {
+    path: '/password/check',
+    element: <EmailCheck />,
+  },
+];
+
+export const publicRouter = [{ path: '/home', element: <Home /> }];
 
 export const privateRouter = [{ path: '/blog', element: <Blog /> }];
