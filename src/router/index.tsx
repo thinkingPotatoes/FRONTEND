c5b2466450de252249d9501d38287d71ee2b897e
kimssumin <ksumin0730@naver.com>
2023-09-05 11:00:07 +0900

    [#21] feat: route 설정 및 svg 설정 변경

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,11 @@
 import Home from '../pages/Home.tsx';
 import Blog from '../pages/Blog.tsx';
+import ReviewDetail from '../pages/ReviewDetail.tsx';
 
-export const commonRouter = [{ path: '/', element: <Home /> }];
+export const commonRouter = [
+  { path: '/', element: <Home /> },
+  { path: '/review/:id', element: <ReviewDetail /> },
+];
 
 export const publicRouter = [];
 
