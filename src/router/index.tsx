a8b0eb476a01cca73ed373f973ab91ef9e7e5ed2
callmejeje <choije401@naver.com>
2023-09-17 22:32:55 +0900

    [#30] feat: 리뷰 작성 화면 라우팅

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,18 @@
 import Blog from '../pages/Blog.tsx';
 import Home from '../pages/Home.tsx';
+import MovieSearch from '../pages/MovieSearch.tsx';
 import PostReview from '../pages/PostReview.tsx';
+import SelectMovie from '../pages/SelectMovie.tsx';
 
-export const commonRouter = [{ path: '/', element: <Home /> }];
+export const commonRouter = [
+  { path: '/', element: <Home /> },
+  { path: '/moviesearch', element: <MovieSearch /> },
+];
 
 export const publicRouter = [];
 
 export const privateRouter = [
   { path: '/blog', element: <Blog /> },
+  { path: '/review', element: <SelectMovie /> },
   { path: '/review/:id', element: <PostReview /> },
 ];
