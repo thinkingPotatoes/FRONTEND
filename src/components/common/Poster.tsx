f7ddfba9febef659cdd281ac7d94ba50472c58bf
callmejeje <choije401@naver.com>
2023-09-17 19:08:35 +0900

    [#30] feat: 포스터 컴포넌트 사이즈 설정

--- src/components/common/Poster.tsx
+++ src/components/common/Poster.tsx
@@ -1,13 +1,33 @@
 import { styled } from 'styled-components';
 
 function Poster({ imgUrl, size }: { imgUrl: string; size: string }) {
-  return <PosterImg src={imgUrl} $size={size} />;
+  let width = 0;
+  let height = 0;
+
+  if (size === 's') {
+    width = 30;
+    height = 42.3;
+  }
+  if (size === 'm') {
+    width = 80;
+    height = 114;
+  }
+  if (size === 'l') {
+    width = 120;
+    height = 170;
+  }
+  if (size === 'xl') {
+    width = 188;
+    height = 286;
+  }
+
+  return <PosterImg src={imgUrl} $width={width} $height={height} />;
 }
 
-const PosterImg = styled.img<{ $size: string }>`
-  min-width: 80px;
-  width: 80px;
-  height: 114px;
+const PosterImg = styled.img<{ $width: number; $height: number }>`
+  min-width: ${(props) => props.$width}px;
+  width: ${(props) => props.$width}px;
+  height: ${(props) => props.$height}px;
 `;
 
 export default Poster;
