06f55cf175089e5c962cfd8bc1663eef16d21e7e
callmejeje <choije401@naver.com>
2023-09-17 21:43:44 +0900

    [#30] feat: 영화 선택 시 작성 페이지 이동

--- src/components/selectMovie/SearchResult.tsx
+++ src/components/selectMovie/SearchResult.tsx
@@ -1,13 +1,25 @@
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 import { MovieResponseList } from '../../types/search';
 import Subtitle1 from '../common/texts/Subtitle1';
 import MovieItem from './MovieItem';
 
 function SearchResult({ searchResults }: { searchResults: MovieResponseList[] }) {
+  const navigate = useNavigate();
+  const handleReview = (id: string) => {
+    navigate(`/review/${id}`);
+  };
+
   return (
     <SearchResultWrapper>
       {searchResults.length > 0 ? (
-        searchResults.map((movieResponse) => <MovieItem movie={movieResponse} />)
+        searchResults.map((movieResponse) => (
+          <MovieItem
+            key={movieResponse.docId}
+            movie={movieResponse}
+            onClick={() => handleReview(movieResponse.docId)}
+          />
+        ))
       ) : (
         <NoResult>
           <Subtitle1>검색 결과가 없습니다</Subtitle1>
@@ -19,13 +31,16 @@ function SearchResult({ searchResults }: { searchResults: MovieResponseList[] })
 
 const SearchResultWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
   padding-top: 24px;
-  height: 52px;
 `;
 
 const NoResult = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 52px;
   color: var(--dark-grey-700);
 `;
 
