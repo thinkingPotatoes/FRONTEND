e5ced2085505073ed45069e099e45ef34a39faf3
kimssumin <ksumin0730@naver.com>
2023-09-18 08:52:03 +0900

    [#31] feat: react-query 전역 설정 및 devtools 추가

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -4,10 +4,7 @@ import RecentSearch from '../components/Search/RecentSearch.tsx';
 import SearchBox from '../components/Search/SearchBox.tsx';
 import ResultSearch from '../components/Search/ResultSearch.tsx';
 
-import { QueryClient, QueryClientProvider } from 'react-query';
 import { MovieResponseList } from '../components/types/search.ts';
-
-const queryClient = new QueryClient();
 
 function SearchPage() {
   const [keyword, setKeyword] = useState('');
@@ -24,31 +21,29 @@ function SearchPage() {
     setDefaultSearch(booleanCheck);
   };
   return (
-    <QueryClientProvider client={queryClient}>
-      <SearchFrame>
-        <SearchBox
-          onSearch={handleSearch}
-          onChange={handleChange}
-          setResults={setSearchResults}
-          keyNow={keyword}
-        />
-        {showDefaultSearch && (
-          <>
-            <RecentSearch
-              onSearch={handleSearch}
-              onChange={handleChange}
-              setResults={setSearchResults}
-              keyNow={keyword}
-            />
-          </>
-        )}
-        {showResult && (
-          <>
-            <ResultSearch results={searchResults} />
-          </>
-        )}
-      </SearchFrame>
-    </QueryClientProvider>
+    <SearchFrame>
+      <SearchBox
+        onSearch={handleSearch}
+        onChange={handleChange}
+        setResults={setSearchResults}
+        keyNow={keyword}
+      />
+      {showDefaultSearch && (
+        <>
+          <RecentSearch
+            onSearch={handleSearch}
+            onChange={handleChange}
+            setResults={setSearchResults}
+            keyNow={keyword}
+          />
+        </>
+      )}
+      {showResult && (
+        <>
+          <ResultSearch results={searchResults} />
+        </>
+      )}
+    </SearchFrame>
   );
 }
 
