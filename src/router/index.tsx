4f061938eb220faa2ab799d30100517cf4dc35d5
callmejeje <choije401@naver.com>
2023-09-22 23:40:22 +0900

    [#30] feat: 리뷰 작성 추가 페이지 라우팅 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,8 @@ import Blog from '../pages/Blog.tsx';
 import Home from '../pages/Home.tsx';
 import MovieSearch from '../pages/MovieSearch.tsx';
 import PostReview from '../pages/PostReview.tsx';
+import PostReviewOption from '../pages/PostReviewOption.tsx';
+import PostReviewRequired from '../pages/PostReviewRequired.tsx';
 import SearchPage from '../pages/SearchPage.tsx';
 import SelectMovie from '../pages/SelectMovie.tsx';
 
@@ -17,4 +19,6 @@ export const privateRouter = [
   { path: '/blog', element: <Blog /> },
   { path: '/review', element: <SelectMovie /> },
   { path: '/review/:id', element: <PostReview /> },
+  { path: '/review/:id/required', element: <PostReviewRequired /> },
+  { path: '/review/:id/option', element: <PostReviewOption /> },
 ];
