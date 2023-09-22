6bdf4f1ec41d9dda7115abd29dabed0ea3b0f8fa
callmejeje <choije401@naver.com>
2023-09-22 22:28:12 +0900

    [#30] feat: 영화 상세 정보 컴포넌트 구현

---
+++ src/components/common/MovieDetails.tsx
@@ -0,0 +1,33 @@
+import { styled } from 'styled-components';
+import { Movie } from '../../types/movie';
+import Body3 from './texts/Body3';
+
+function MovieDetails({ movie }: { movie: Movie }) {
+  const details = [movie.prodYear, movie.nation, movie.rating, movie.genre];
+  return (
+    <Info>
+      {details.map((detail, idx) => {
+        if (detail.length === 0) return;
+        return (
+          <Detail key={detail}>
+            <Body3>{detail}</Body3>
+            {idx !== details.length - 1 && <Body3>|</Body3>}
+          </Detail>
+        );
+      })}
+    </Info>
+  );
+}
+
+const Info = styled.div`
+  display: flex;
+  gap: 4px;
+  color: var(--dark-grey-500);
+`;
+
+const Detail = styled.div`
+  display: flex;
+  gap: 4px;
+`;
+
+export default MovieDetails;
