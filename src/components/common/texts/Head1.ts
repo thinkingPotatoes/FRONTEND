a41a0c12f21c7c6048df4559b5995fc0b3912ba0
이국신 <nw4611@naver.com>
2023-09-24 00:46:42 +0900

    [#32] feat: Head1의 Prop 추가 글자 색상과 margin-bottom  속성 추가

--- src/components/common/texts/Head1.ts
+++ src/components/common/texts/Head1.ts
@@ -1,11 +1,18 @@
 import styled from 'styled-components';
 
-const Head1 = styled.div`
+type Props = {
+  color?: string;
+  marginBottom?: string;
+};
+
+const Head1 = styled.div<Props>`
   font-size: 24px;
   font-style: normal;
   font-weight: 600;
   line-height: 130%; /* 31.2px */
   letter-spacing: -0.24px;
+  color: ${({ color }: Props) => color ?? ''};
+  margin-bottom: ${({ marginBottom }: Props) => marginBottom ?? ''};
 `;
 
 export default Head1;
