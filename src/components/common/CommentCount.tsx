039a3d45c3c632fc7be1fef7d51a004c0a71f91a
callmejeje <choije401@naver.com>
2023-09-26 18:16:37 +0900

    [#14] feat: Body3 공통 컴포넌트 적용

--- src/components/common/CommentCount.tsx
+++ src/components/common/CommentCount.tsx
@@ -1,11 +1,12 @@
 import { styled } from 'styled-components';
 import { ReactComponent as CommentIcon } from '../../assets/image/icon/comment.svg';
+import Body3 from './texts/Body3';
 
 function CommentCount({ count }: { count: number }) {
   return (
     <CommentCountWrapper>
       <CommentIcon />
-      <Count>{count}</Count>
+      <Body3>{count}</Body3>
     </CommentCountWrapper>
   );
 }
@@ -14,17 +15,7 @@ const CommentCountWrapper = styled.div`
   display: flex;
   align-items: center;
   gap: 2px;
-`;
-
-const Count = styled.span`
-  color: var(--dark-grey-700, #c3c3c6);
-
-  /* Body3 */
-  font-family: Pretendard;
-  font-size: 12px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 130%; /* 15.6px */
+  color: var(--dark-grey-700);
 `;
 
 export default CommentCount;
