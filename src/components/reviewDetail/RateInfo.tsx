fe1f849536495136473b344c61c382d94e7566a3
kimssumin <ksumin0730@naver.com>
2023-10-03 20:45:22 +0900

    [#21] fix: rating 별 반개 구현

--- src/components/reviewDetail/RateInfo.tsx
+++ src/components/reviewDetail/RateInfo.tsx
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
 import { ReactComponent as BlankStarSvg } from '../../assets/image/icon/blankStar.svg';
 import { ReactComponent as FillStarSvg } from '../../assets/image/icon/fillStar.svg';
+import { Rating } from 'react-simple-star-rating';
 
 interface Props {
   rate: number;
@@ -9,26 +10,19 @@ interface Props {
 const RateInfo = ({ rate }: Props) => {
   return (
     <RateWrapper>
-      {Array.from({ length: 5 }, (_, index) => (
-        <StarIcon key={index} filled={index < rate} />
-      ))}
+      <Rating
+        readonly
+        initialValue={rate}
+        fillIcon={<FillStarSvg width={24} height={24} />}
+        emptyIcon={<BlankStarSvg width={24} height={24} />}
+        allowFraction={true}
+      />
     </RateWrapper>
   );
 };
 
-interface StarIconProps {
-  filled: boolean;
-}
-
 const RateWrapper = styled.div`
   // margin: 6px 0px;
-`;
-
-const StarIcon = styled(({ filled }: StarIconProps) =>
-  filled ? <FillStarSvg /> : <BlankStarSvg />,
-)`
-  width: 24px;
-  height: 24px;
 `;
 
 export default RateInfo;
