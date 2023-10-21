639492f896163597a81e1cbd48dab1ecb7aa301c
callmejeje <choije401@naver.com>
2023-10-22 02:01:02 +0900

    [#45] feat: post 메서드의 검색 api 사용 가능하도록 수정

--- src/hooks/useInfinteScroll.tsx
+++ src/hooks/useInfinteScroll.tsx
@@ -2,7 +2,13 @@ import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
 import axios from '../api/apiController';
 
-const useInfinteScroll = <T,>(path: string) => {
+const useInfinteScroll = <T,>(path: string, keyword: string) => {
+  type List = {
+    totalCnt: number;
+    curPage: number;
+    list: T[];
+  };
+
   const { ref, inView } = useInView();
 
   const [list, setList] = useState<T[]>([]);
@@ -11,6 +17,30 @@ const useInfinteScroll = <T,>(path: string) => {
   const [totalCount, setTotalCount] = useState(-1);
   const [loading, setLoading] = useState(true);
 
+  const params = {
+    page,
+    size: 10,
+    sort: `${sort},desc`,
+  };
+
+  const commonOptions = {
+    url: path,
+    params,
+  };
+
+  const getOptions = {
+    ...commonOptions,
+    method: 'get',
+  };
+
+  const postOptions = {
+    ...commonOptions,
+    method: 'post',
+    data: { keyword },
+  };
+
+  const options = keyword.length > 0 ? postOptions : getOptions;
+
   const readMoreList = () => {
     if (loading) return;
     if (totalCount === list.length) return;
@@ -18,27 +48,27 @@ const useInfinteScroll = <T,>(path: string) => {
     setPage(page + 1);
   };
 
+  const renewList = (data: List) => {
+    if (!data || data.curPage === 0) setList(data.list);
+    else setList([...list, ...data.list]);
+    setTotalCount(data.totalCnt);
+    setLoading(false);
+  };
+
   useEffect(() => {
     setPage(0);
     setList([]);
-  }, [sort]);
+  }, [sort, keyword]);
 
   useEffect(() => {
-    if (totalCount === list.length) return;
+    if (totalCount === list.length && keyword.length === 0) return;
     setLoading(true);
 
-    axios
-      .get(path, {
-        params: { page, size: 10, sort: `${sort},desc` },
-      })
-      .then((data) => {
-        const responseList = data.data.data.list;
-        if (data.data.data.curPage === 0) setList(responseList);
-        else setList([...list, ...responseList]);
-        setTotalCount(data.data.data.totalCnt);
-        setLoading(false);
-      });
-  }, [page, sort]);
+    axios.request(options).then((data) => {
+      if (data.data.data === null) return;
+      renewList(data.data.data);
+    });
+  }, [page, sort, keyword]);
 
   useEffect(() => {
     if (!inView) return;
