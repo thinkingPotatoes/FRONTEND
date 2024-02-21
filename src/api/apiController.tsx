b625775590a537b6fdc0ca438cbc421484fda212
jueun_choi <jueun_choi2@tmax.co.kr>
2024-02-21 19:58:06 +0900

    refactor: 사용하지 않는 환경변수 주석 처리

--- src/api/apiController.tsx
+++ src/api/apiController.tsx
@@ -1,6 +1,6 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 
-const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
+// const ACCESS_TOKEN = import.meta.env.VITE_ACCESS_TOKEN;
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const instance = axios.create({
